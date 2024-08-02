efce76b6887c4282f16f487b36e832901d041348
Chris Flipse <cflipse@gmail.com>
2024-08-02 15:26:35 -0400

    enable feed generator

--- config/initializers.rb
+++ config/initializers.rb
@@ -42,6 +42,8 @@ Bridgetown.configure do |config|
   # init :ssr
   #
 
+  init :"bridgetown-feed"
+
   # Uncomment to use file-based dynamic template routing via Roda (make sure you
   # uncomment the gem dependency in your `Gemfile` as well):
   #
